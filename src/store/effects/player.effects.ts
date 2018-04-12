dd3492c90967b9db5a0944a3b209f53d70250218
Liam Muller <liam.w.muller@gmail.com>
2018-04-12 19:36:29 -0400

    Move getPlayer to my own RESTfull api

--- src/store/effects/player.effects.ts
+++ src/store/effects/player.effects.ts
@@ -1,25 +1,25 @@
-import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { MainPage } from "../../pages/pages";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MainPage } from '../../pages/pages';
 
-import { Actions, Effect } from "@ngrx/effects";
-import { Action } from "@ngrx/store";
+import { Actions, Effect } from '@ngrx/effects';
+import { Action } from '@ngrx/store';
 import {
   PlayerActionTypes,
   LoadPlayer,
   LoadPlayerSuccess,
   LoadPlayerFailure
-} from "../actions/player.actions";
-import { Observable } from "rxjs/Observable";
-import { switchMap, map, concatMap, catchError } from "rxjs/operators";
-import { of } from "rxjs/observable/of";
+} from '../actions/player.actions';
+import { Observable } from 'rxjs/Observable';
+import { switchMap, map, concatMap, catchError } from 'rxjs/operators';
+import { of } from 'rxjs/observable/of';
 import {
   ToastController,
   App,
   LoadingController,
   Loading
-} from "ionic-angular";
-import { LoadMatches } from "..";
+} from 'ionic-angular';
+import { LoadMatches } from '../actions/matches.actions';
 
 export interface PlayerResponse {
   player: any;
@@ -27,7 +27,6 @@ export interface PlayerResponse {
 
 @Injectable()
 export class PlayerEffects {
-  headers: HttpHeaders;
   loading: Loading;
 
   constructor(
@@ -37,18 +36,12 @@ export class PlayerEffects {
     private toastCtrl: ToastController,
     private loadingCtrl: LoadingController
   ) {
-    this.headers = new HttpHeaders()
-      .set("Accept", "application/vnd.api+json")
-      .set(
-        "Authorization",
-        "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIyYzI4Njg1MC0xOGNmLTAxMzYtZTdjMy0wMzMxODI1NzdmN2YiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTIyNjkyNjgyLCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6InB1Ymctdmlld2VyIiwic2NvcGUiOiJjb21tdW5pdHkiLCJsaW1pdCI6MTB9.-W2PdClWJoDPNuSp1lA-45YPZkQLCGJbLiZOD5ouZ6s"
-      );
     this.loading = this.createLoader();
   }
 
   createLoader(): Loading {
     return this.loadingCtrl.create({
-      content: "Searching Player..."
+      content: 'Searching Player...'
     });
   }
 
@@ -65,10 +58,9 @@ export class PlayerEffects {
         this.loading.present();
         return this.http
           .get<any>(
-            `https://api.playbattlegrounds.com/shards/${player.platform}-${
+            `http://pubgapi.lmuller.me/api/player/${player.username}?region=${
               player.region
-            }/players?filter[playerNames]=${player.username}`,
-            { headers: this.headers }
+            }&platform=${player.platform}`
           )
           .pipe(
             concatMap(value => {
@@ -82,11 +74,11 @@ export class PlayerEffects {
                 this.app.getActiveNav().push(MainPage);
               }
               return [
-                new LoadPlayerSuccess(value.data[0]),
+                new LoadPlayerSuccess(value.player),
                 new LoadMatches({
                   platform: player.platform,
                   region: player.region,
-                  matches: value.data[0].relationships.matches.data
+                  matches: value.player.matches
                 })
               ];
             }),
@@ -96,9 +88,9 @@ export class PlayerEffects {
                 this.loading = null;
               }
               let toast = this.toastCtrl.create({
-                message: "Player was not found",
+                message: 'Player was not found',
                 duration: 3000,
-                position: "top"
+                position: 'top'
               });
 
               toast.present();
--- src/store/selectors/player.selectors.ts
+++ src/store/selectors/player.selectors.ts
@@ -1,28 +1,13 @@
-import { createSelector } from "@ngrx/store";
-import * as fromViewer from "../reducers";
-import * as fromPlayer from "../reducers/player.reducer";
+import { createSelector } from '@ngrx/store';
+import * as fromViewer from '../reducers';
+import * as fromPlayer from '../reducers/player.reducer';
 
 export const getPlayerState = createSelector(
   fromViewer.getViewerState,
   (state: fromViewer.ViewerState) => state.player
 );
 
-export const getRawPlayer = createSelector(
-  getPlayerState,
-  fromPlayer.getPlayer
-);
-
-export const getPlayer = createSelector(getRawPlayer, rawPlayer => {
-  console.log("raw player: ", rawPlayer);
-  if (!rawPlayer) return undefined;
-  const player = {
-    name: rawPlayer.attributes.name,
-    id: rawPlayer.id,
-    matches: rawPlayer.relationships.matches.data
-  };
-  console.log("formatted player: ", player);
-  return player;
-});
+export const getPlayer = createSelector(getPlayerState, fromPlayer.getPlayer);
 
 export const getPlayerLoading = createSelector(
   getPlayerState,
