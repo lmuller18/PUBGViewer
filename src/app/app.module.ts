a5a1ae6bd1b929c84dc2ce12fb6b6bf0d0c59943
Liam Muller <liam.w.muller@gmail.com>
2018-04-18 22:26:31 -0400

    Material Design on match details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { HttpClientModule } from "@angular/common/http";
-import { ErrorHandler, NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { Camera } from "@ionic-native/camera";
-import { SplashScreen } from "@ionic-native/splash-screen";
-import { StatusBar } from "@ionic-native/status-bar";
-import { IonicStorageModule, Storage } from "@ionic/storage";
-import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
+import { HttpClientModule } from '@angular/common/http';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { Camera } from '@ionic-native/camera';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { IonicStorageModule, Storage } from '@ionic/storage';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { StoreModule } from "@ngrx/store";
-import { reducers, effects } from "../store";
-import { EffectsModule } from "@ngrx/effects";
+import { StoreModule } from '@ngrx/store';
+import { reducers, effects } from '../store';
+import { EffectsModule } from '@ngrx/effects';
 
-import { Items } from "../mocks/providers/items";
-import { Settings } from "../providers/providers";
-import { Api } from "../providers/providers";
-import { MyApp } from "./app.component";
+import { Items } from '../mocks/providers/items';
+import { Settings } from '../providers/providers';
+import { Api } from '../providers/providers';
+import { MyApp } from './app.component';
 
 // The translate loader needs to know where to load i18n files
 // in Ionic's static asset pipeline.
@@ -28,22 +28,22 @@ export function provideSettings(storage: Storage) {
    */
   return new Settings(storage, {
     option1: true,
-    option2: "Ionitron J. Framework",
-    option3: "3",
-    option4: "Hello"
+    option2: 'Ionitron J. Framework',
+    option3: '3',
+    option4: 'Hello'
   });
 }
 
 @NgModule({
   declarations: [MyApp],
   imports: [
-    BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     IonicModule.forRoot(MyApp),
     IonicStorageModule.forRoot(),
     StoreModule.forRoot(reducers),
     EffectsModule.forRoot(effects),
-    StoreModule.forFeature("viewer", reducers),
+    StoreModule.forFeature('viewer', reducers),
     EffectsModule.forFeature(effects)
   ],
   bootstrap: [IonicApp],
--- src/pages/match-details/match-details.module.ts
+++ src/pages/match-details/match-details.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
-
 import { MatchDetailsPage } from './match-details';
+import { MatExpansionModule } from '@angular/material';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [MatchDetailsPage],
-  imports: [IonicPageModule.forChild(MatchDetailsPage)],
+  imports: [
+    IonicPageModule.forChild(MatchDetailsPage),
+    MatExpansionModule,
+    MatListModule,
+    MatIconModule
+  ],
   exports: [MatchDetailsPage]
 })
 export class MatchDetailsPageModule {}
