1cbc830468ce7ac1f853fb97e791ad0df0524adc
Liam Muller <liam.w.muller@gmail.com>
2018-04-22 20:22:13 -0400

    Show attack and victim locations on click of attack

--- src/pages/match-details/match-details.ts
+++ src/pages/match-details/match-details.ts
@@ -14,6 +14,10 @@ export class MatchDetailsPage implements OnInit {
   telemetry$: Observable<any>;
   telemetry: any;
   match: any;
+  mapsize = 819200;
+
+  canvas: any;
+  ctx: any;
 
   constructor(
     public navCtrl: NavController,
@@ -41,17 +45,51 @@ export class MatchDetailsPage implements OnInit {
   ngOnInit() {
     this.telemetry$.subscribe(telemetry => {
       this.telemetry = telemetry;
-      // this.drawMap();
     });
+    this.canvas = this.elementRef.nativeElement.ownerDocument.getElementById(
+      'map'
+    );
+    this.ctx = this.canvas.getContext('2d');
+    this.ctx.canvas.width = window.innerWidth;
+    this.ctx.canvas.height = 250;
+    // this.drawMap();
   }
 
   drawMap() {
-    const canvas = this.elementRef.nativeElement.ownerDocument.getElementById(
-      'map'
-    );
-    const ctx = canvas.getContext('2d');
+    this.ctx.fillStyle = 'green';
+    this.ctx.fillRect(10, 10, 100, 100);
+  }
 
-    ctx.fillStyle = 'green';
-    ctx.fillRect(10, 10, 100, 100);
+  drawAttack(attack) {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    const attackX =
+      attack.attacker.location.x / this.mapsize * this.canvas.width;
+    const attackY =
+      attack.attacker.location.y / this.mapsize * this.canvas.width;
+    const victimX = attack.victim.location.x / this.mapsize * this.canvas.width;
+    const victimY = attack.victim.location.y / this.mapsize * this.canvas.width;
+
+    console.log(`(${this.canvas.width}, ${this.canvas.height})`);
+
+    console.log(`(${attackX}, ${attackY}`);
+    console.log(`(${victimX}, ${victimY}`);
+
+    this.ctx.fillStyle = 'green';
+    this.ctx.beginPath();
+    this.ctx.arc(attackX, attackY, 5, 0, 2 * Math.PI);
+    this.ctx.closePath();
+    this.ctx.fill();
+
+    this.ctx.fillStyle = 'red';
+    this.ctx.beginPath();
+    this.ctx.arc(victimX, victimY, 5, 0, 2 * Math.PI);
+    this.ctx.closePath();
+    this.ctx.fill();
+
+    this.ctx.setLineDash([5, 3]); /*dashes are 5px and spaces are 3px*/
+    this.ctx.beginPath();
+    this.ctx.moveTo(attackX, attackY);
+    this.ctx.lineTo(victimX, victimY);
+    this.ctx.stroke();
   }
 }
