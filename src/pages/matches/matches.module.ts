5718b35b5ecf24563ceda404de8095453ac3e3e4
Liam Muller <liam.w.muller@gmail.com>
2018-06-01 20:37:03 -0400

    Match Details Screen - Restyle first pass

--- src/pages/matches/matches.module.ts
+++ src/pages/matches/matches.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from "@angular/core";
-import { IonicPageModule } from "ionic-angular";
-
-import { MatchesPage } from "./matches";
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatchesPage } from './matches';
 
 @NgModule({
   declarations: [MatchesPage],
-  imports: [IonicPageModule.forChild(MatchesPage)],
+  imports: [
+    IonicPageModule.forChild(MatchesPage),
+    MatCardModule,
+    MatListModule,
+    MatButtonModule
+  ],
   exports: [MatchesPage]
 })
 export class ListMasterPageModule {}
